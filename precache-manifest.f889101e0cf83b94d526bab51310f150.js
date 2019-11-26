d812b24cc898a087bc9695aa6919fe7688ab0aca
Dung Nguyen <dung.nguyen@pricefx.eu>
2019-11-26 16:23:53 +0700

    Updates

---
+++ precache-manifest.f889101e0cf83b94d526bab51310f150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24826862c7994d5bf1d4449cd70080e7",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "7635da435a7cd8529732",
+    "url": "/todoList/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "e549981b8ab670b673a6",
+    "url": "/todoList/static/css/main.8d591a61.chunk.css"
+  },
+  {
+    "revision": "7635da435a7cd8529732",
+    "url": "/todoList/static/js/2.f696e9d3.chunk.js"
+  },
+  {
+    "revision": "e549981b8ab670b673a6",
+    "url": "/todoList/static/js/main.1d0a014b.chunk.js"
+  },
+  {
+    "revision": "315bf5cb919363d333a3",
+    "url": "/todoList/static/js/runtime-main.1a0e5b74.js"
+  }
+]);
